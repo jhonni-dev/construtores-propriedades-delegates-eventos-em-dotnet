afdfc9814acba030ec875724de6d0e0ecf6a0802
jhonni-dev <jhonni.dev@gmail.com>
2022-01-06 09:31:43 -0300

    Construtor privado

--- ExemploConstrutores/Program.cs
+++ ExemploConstrutores/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Pessoa p1 = new Pessoa();
-        p1.Apresentar();
+        Log log = Log.GetInstance();
+        log.PropriedadeLog = "Teste instância";
+        Log log2 = Log.GetInstance();
+        System.Console.WriteLine(log2.PropriedadeLog);
+        // Pessoa p1 = new Pessoa();
+        // p1.Apresentar();
     }
 }
